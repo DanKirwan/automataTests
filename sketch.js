cb904c4ab98b4572f6c92f6ad3a669646db84557
Dan <dangeorgek@gmail.com>
2019-08-30 15:04:36 +0100

    Branching starting to work

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-var numSoldiers = 11;
+var numSoldiers = 41;
 
 /*var soldiers = [-1]
 for(i = 0; i < numSoldiers; i++) {
@@ -6,11 +6,15 @@ for(i = 0; i < numSoldiers; i++) {
 }
 soldiers.push(-1);*/
 
-var soldiers = [-1,0,0,0,0,0,3,0,0,0,0,0,-1]
+var soldiers = [-1];
+for(i = 0; i < 20; i++) soldiers.push(0);
+soldiers.push(3);
+for(i = 0; i < 20; i++) soldiers.push(0);
+soldiers.push(-1);
 
 var layers = [];
 layers.push(soldiers)
-for(i = 0; i < numSoldiers; i++) {
+for(i = 0; i < 50; i++) {
   var o = layers[i]
   var n = [-1];
   for(j = 1; j < numSoldiers + 1; j++) {
@@ -24,7 +28,7 @@ for(i = 0; i < numSoldiers; i++) {
 
 function setup() {
   colorMode(HSB,100)
-  createCanvas(720, 540);
+  createCanvas(numSoldiers * 10, layers.length*10);
 
 
 }
@@ -35,24 +39,51 @@ function draw() {
   for(x = 0; x < layers.length; x++) {
     for(i = 1; i < numSoldiers+1; i++) {
       fill(layers[x][i] * 10, 100, 100);
-      rect((width/split) * (i + 1), (height/ (layers.length + 2)) * (x + 1),
-            (width/split), (height/layers.length));
+      rect((width/split) * (i + 1), (width/split) * (x + 1),
+            (width/split), (width/split));
     }
   }
 
 }
 
 
-function basicAutomata(left, me, right) {
+function basicAutomata(l, c, r) {
 
-
-  var val = "" + left + "" + me + "" + right;
+  var val = "" + l + "" + c + "" + r;
 
   if(val == "100" || val == "001") return 1;
-  if(val == "300" || val == "003") return 1;
-  if(me == 1) return 2;
-  if(me == 3) return 4;
-  if(me == 4) return 3;
+  if(val == "300" || val == "003") return 1; //3 is M
+  if(val == "-101" || val == "10-1") return 6;
+  if(val == "030") return 3;
+  if(val == "131") return 2;
+  if(val == "221" || val == "122") return 5;
+  if(val == "052" || val == "250") return 4;
+  if(val == "042" || val == "240") return 4;
+  if(val == "204" || val == "004" || val == "402" || val == "400") return 5;
+  if(val == "040") return 4;
+  if(val == "054" || val == "450") return 5;
+  if(val == "050") return 4;
+  if(val == "540" || val == "045" || val == "542" || val == "245") return 2;
+  if(val == "520" || val == "025" || val == "522" || val == "225") return 2;
+  if(val == "420" || val == "024") return 2;
+
+  if(val == "013" || val == "310") return 2;
+  if(val == "012" || val == "210") return 2; //2 is the barrier to maintain 1's direction
+
+  //Reflection
+  if(val == "-162" || val == "26-1") return 1;
+  if(val == "-110" || val == "01-1") return 2;
+
+  //Intersection
+  if(val == "104" || val == "401") return 3;
+  if(val == "234" || val == "432") return 3;
+  if(val == "501" || val == "105") return 3;
+  if(val == "235" || val == "532") return 3;
+
+  if(val == "302" || val == "203") return 1;
+
+
+//  if(me == 5) return 1; //This is the edge case, 5 acts as R
 
 
 
